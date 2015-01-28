ac9c3af9fc47d4b824988d08fe56b48d0c1ae580
sjbealmer <sjbealmer@wacohi.net>
2015-01-27 19:02:32 -0600

    Autonomous with encoders 2.0 (less errors)

--- src/org/usfirst/frc5442/Fred/commands/AutonomousCommand.java
+++ src/org/usfirst/frc5442/Fred/commands/AutonomousCommand.java
@@ -36,7 +36,7 @@ public class  AutonomousCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	While (Robot.encoders.quadratureEncoderLeft() < 10)
+    	while (Robot.encoders.quadratureEncoderLeft.get() < 10)
     	{
     		
     	}
